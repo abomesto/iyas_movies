a716877ccda4ed3ead272fa956d43ae750970c77
abomesto <eyas.adi.88@gmail.com>
2023-11-03 12:36:19 +0100

    Added Express
    Added express to the project

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-const express = require('express');
+const express = require('express'),
+morgan = require('morgan');
+fs = require('fs'), // import built in node modules fs and path 
+  path = require('path');
+
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+let topMovies = [
+    {
+      title: 'Into the Wild',
+      director: 'Sean Penn'
+    },
+    {
+      title: 'Amélie',
+      director: 'Jean-Pierre Jeunet'
+    },
+    {
+      title: 'Before Sunrise',
+      director: 'Richard Linklater'
+    }
+  ];
+
+  // GET requests
+
+  app.get('/topMovies', (req, res) => {
+    res.json(topMovies);
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to Iyas top Movies');
+  });
+
+  app.use('/public/documentation.html', express.static('public'));
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
